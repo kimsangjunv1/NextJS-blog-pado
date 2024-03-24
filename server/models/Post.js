b3224ee5938b0258e7709718873507da06477a41
kimsangjunv1 <kimsangjunv1>
2024-03-25 01:43:46 +0900

    Improve: 게시물 정보 스키마 생성

---
+++ server/models/Post.js
@@ -0,0 +1,32 @@
+// 몽구스 변수에 할당
+const mongoose = require("mongoose");
+
+// 유저 스키마 생성
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
